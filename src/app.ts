9b61fe725229a4938a620caa6ba3850555204ec8
Marlon Tornes Torres <marlon07021@gmail.com>
2020-02-25 14:47:33 -0600

    [ADD] app.ts.

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import * as express from 'express'
+import { Application } from 'express'
+
+class App {
+    public app: Application
+    public port: number
+
+    constructor(appInit: {
+        port: number;
+        middleWares: any;
+        controllers: any;
+
+    }) {
+        this.app = express()
+        this.port = appInit.port
+
+        this.middlewares(appInit.middleWares)
+        this.routes(appInit.controllers)
+    }
+
+    private middlewares(middleWares: { forEach: (arg0: (middleware: any) => void) => void; }) {
+        middleWares.forEach(
+            middleware => {
+                this.app.use(middleware)
+            }
+        );
+    }
+
+    private routes(controllers: {forEach: (arg0: (controller: any) => void) => void;}) {
+        controllers.forEach(controller => {
+            this.app.use('/', controller.router)
+        })
+    }
+
+    private assets() {
+        this.app.use(express.static('public'))
+        this.app.use(express.static('views'))
+    }
+
+    private template() {
+        this.app.set('view engine', 'pug')
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`App listening on the http://localhost:${this.port}`)
+        })
+    }
+}
