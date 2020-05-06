de482e1c813788f59448fbbe4b79774b0d1aa292
Marlon Tornes Torres <marlon07021@gmail.com>
2020-05-06 14:07:19 -0500

    [MDF] Separate controllers into exported array.

--- src/bootstrapper.ts
+++ src/bootstrapper.ts
@@ -4,20 +4,13 @@ import * as bodyParser from 'body-parser'
 import * as helmet from 'helmet'
 import * as cors from 'cors'
 import loggerMiddleware from './middleware/logger'
-
-import jsonifyPlugin from "./plugins/jsonify";
-
-import HomeController from './controllers/home.controller'
-import UserController from './controllers/user.controller'
+import * as controllers from './controllers'
 
 import Constants = require("./config/constants/constants");
 
 const app = new App({
     port: Constants.NODE_PORT,
-    controllers: [
-        new HomeController(),
-        new UserController()
-    ],
+    controllers: controllers,
     middleWares: [
         cors(),
         helmet(),
---
+++ src/controllers.ts
@@ -0,0 +1,7 @@
+import HomeController from './controllers/home.controller'
+import UserController from './controllers/user.controller'
+
+export default [
+    new HomeController(),
+    new UserController()
+]
